64500a43bbd9e27ced4d8428df8e3b6f5c8490ff
Sang Hoon Kim <sanghoonkim@uchicago.edu>
2020-06-11 16:04:26 -0500

    fixed bugs in cb_demo.py

--- CB_demo/cb_demo.py
+++ CB_demo/cb_demo.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from matplotlib import pyplot as plt
 
+# Set a random seed
 np.random.seed(0)
 
 # Initialize an environment
@@ -17,6 +18,20 @@ reward_table = {user: np.random.choice(10, size=num_ads) for user in range(num_u
 
 # Implement IPS 
 class IPS:
+    def __init__(self):
+        self.estimates = list()
+
+    def get_estimate(self):
+        """
+        Append the IPS estimate of a single sample to self.estimates.
+        You should implement this function
+        """
+
+    def evaluation(self, num_samples):
+        """
+        Get the mean of num_samples number of IPS estimates
+        """
+        return np.mean(self.estimates[:num_samples])
 
 # Three policies    
 class UniformlyRandom:
@@ -26,7 +41,7 @@ class UniformlyRandom:
         """
         self.num_actions = num_actions 
 
-    def choose_action(self, context ):
+    def choose_action(self, context):
         """
         return a chosen action and action-probabilities
         """
@@ -43,7 +58,7 @@ class UniformlyRandom:
         pass
 
 class EpsilonGreedy:
-    def __init__(self, num_actions, num_contexts, epsilon=0.2):
+    def __init__(self, num_actions, num_contexts, epsilon=0.1):
         """
         This is a CB verision of epsilon greedy
 
@@ -66,6 +81,7 @@ class EpsilonGreedy:
         self.num_contexts = num_contexts
         self.num_actions_chosen = {context: np.ones(num_actions) for context in range(num_contexts)}
         self.sum_rewards = {context: np.zeros(num_actions) for context in range(num_contexts)}
+        self.epsilon = epsilon
 
     def choose_action(self, context):
         """
@@ -73,7 +89,7 @@ class EpsilonGreedy:
         """
         # Check which action is the best action 
         # The best action is the action that has the highest total reward for a given context
-        best_action = np.argmax(self.sum_rewards / self.num_actions_chosen[context])
+        best_action = np.argmax(self.sum_rewards[context] / self.num_actions_chosen[context])
 
         # Get action probabilities based on the best action
         # there is a (epsilon + epsilon/num_actions) probability of choosing the best action.
@@ -125,19 +141,19 @@ class DeterministicPolicy:
         pass
 
 # Initialize policies
-epsilon = 0.3 # epsilon is a tunable parameter
+epsilon = 0.1 # epsilon is a tunable parameter
 
 uni_ran = UniformlyRandom(num_ads)
 eps_greedy = EpsilonGreedy(num_ads, num_users, epsilon=epsilon)
 
 # Start the demo
 num_samples = 1000000 # total number of samples. Feel free to change this if you want to
-trace = list() # list to store each CB tuple of (context, chosen action, action probability, reward revealed)
 new_policy_true_rewards = list() # Keep track of true rewards revealed by the new policy
 
 # Implement the demo as specified by the comments below
-old_policy = 
-new_policy = 
+old_policy = eps_greedy 
+new_policy = uni_ran
+ips_estimator = IPS()
 
 for sample_index in range(num_samples): 
     # Context (user) revealed
@@ -153,17 +169,15 @@ for sample_index in range(num_samples):
     new_chosen_action, new_probabilities = new_policy.choose_action(user)
 
     # Record the reward revealed by the new policy to get the true performance of the new policy
-    new_policy_true_rewards.append(new_chosen_action)
+    new_policy_true_rewards.append(reward_table[user][new_chosen_action])
 
     # Store the CB sample in trace for evaluation
-    trace.append((user, 
-                  old_chosen_action, 
-                  old_probabilities[old_chosen_action], 
-                  new_probability[new_chosen_action], 
-                  reward))
+    ips_estimator.get_estimate("Implement")
 
     # Update the old policy if needed
-    old_policy.update((user, old_chosen_action, reward_revealed))
+    old_policy.update((user, old_chosen_action, reward))
+
+
 
 # The expected value of the reward revealed by the new policy
 # Actually, this is not the "true" expected value but a value 
@@ -172,4 +186,5 @@ for sample_index in range(num_samples):
 # the true expected value using the environment defined at the beginning.
 true_performance = np.mean(new_policy_true_rewards)
 
-# Implement a code to get IPS estimates using the trace
+# Now you have the list of every IPS estimate you have got in self.estimates of ips_estimator.
+# Use the list to solve the problems.
